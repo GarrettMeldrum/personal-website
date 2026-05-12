9855af4dc18b986fa6ef6f5495819cd3570769d5
GarrettMeldrum <garrettmeldrum@icloud.com>
2026-05-12 09:11:24 -0600

    testing oauth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+import { handlers } from "@/auth";
+
+// boilerplate from the Auth.js website
+// Add a Route Handler under /app/api/auth/[...nextauth]/route.ts.
+export const { GET, POST } = handlers;
