c5fdbb73675b03479cde8d87bf029cb8159122f3
GarrettMeldrum <garrettmeldrum@icloud.com>
2026-05-12 11:22:08 -0600

    another round of changes

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -3,19 +3,15 @@ import SignIn from "@/components/sign-in";
 
 export default async function Page() {
   const session = await auth();
+  console.log("session:", JSON.stringify(session));
 
-  if (!session) {
+  if (!session?.user) {
     return <SignIn />;
   }
-
-  const isAdmin =
-    session.user?.email == "garrettmeldrum14@gmail.com" ||
-    session.user?.email == "malorie2000@hotmail.com";
 
   return (
     <div>
       <h1>Photos</h1>
-      {isAdmin && <div>{/* Upload/delete controls */}</div>}
       {/* gallery */}
     </div>
   );
