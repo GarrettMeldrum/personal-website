fd9907646a6c8ffca633096204d1ec3443de4165
GarrettMeldrum <garrettmeldrum@icloud.com>
2026-05-12 11:14:58 -0600

    additional oauth work

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <div></div>;
+import { auth } from "@/auth";
+import SignIn from "@/components/sign-in";
+
+export default async function Page() {
+  const session = await auth();
+
+  if (!session) {
+    return <SignIn />;
+  }
+
+  const isAdmin =
+    session.user?.email == "garrettmeldrum14@gmail.com" ||
+    session.user?.email == "malorie2000@hotmail.com";
+
+  return (
+    <div>
+      <h1>Photos</h1>
+      {isAdmin && <div>{/* Upload/delete controls */}</div>}
+      {/* gallery */}
+    </div>
+  );
 }
